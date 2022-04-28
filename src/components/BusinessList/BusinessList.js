f0ec13132916ee85fe237c509126a59f065ddd93
Алексей <alexburnitdown@gmail.com>
2022-04-28 17:04:07 +0300

    all works 2

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -2,16 +2,14 @@ import React from "react";
 import "./BusinessList.css";
 import Business from "../Business/Business.js";
 
-class BusinessList extends React.Component {
-  render() {
-    return (
-      <div className="BusinessList">
-        {this.props.places.map((place) => {
-          return <Business place={place} key={place.id} />;
-        })}
-      </div>
-    );
-  }
+function BusinessList({places}) {
+  return (
+    <div className="BusinessList">
+      {places.map((place) => {
+        return <Business place={place} key={place.id} />;
+      })}
+    </div>
+  );
 }
 
 export default BusinessList;
--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,26 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from "react-responsive-carousel";
 import "./Carousel.css";
 
-class CarouselComponent extends Component {
-  render() {
-    return (
-      <Carousel showThumbs={true} className="carousel-container">
-        {this.props.photos &&
-          this.props.photos.map((photo, index) => {
-            return (
-              <div
-                key={index}
-                className="carousel-photo"
-                style={{ backgroundImage: `url(${photo})` }}
-                alt="main logo"
-              ></div>
-            );
-          })}
-      </Carousel>
-    );
-  }
+function CarouselComponent({photos}) {
+  return (
+    <Carousel showThumbs={true} className="carousel-container">
+      {photos &&
+        photos.map((photo, index) => {
+          return (
+            <div
+              key={index}
+              className="carousel-photo"
+              style={{ backgroundImage: `url(${photo})` }}
+              alt="main logo"
+            ></div>
+          );
+        })}
+    </Carousel>
+  );
 }
 
 export default CarouselComponent;
